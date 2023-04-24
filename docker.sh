024a7824932f531f060b649daa378488a3cb5999
Simon Schmeisser <s.schmeisser@gmx.net>
2023-04-24 21:20:11 +0200

    apply patches

--- docker.sh
+++ docker.sh
@@ -5,6 +5,6 @@ set -ex
 mkdir -p build
 cd build
 cp -r /workspace/* .
-mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build
+mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build -p
 sudo mkdir -p /workspace/RPMS
 sudo cp -r RPMS/*.rpm /workspace/RPMS
